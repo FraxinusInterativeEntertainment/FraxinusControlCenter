e613f1fb715668563fa33ce4d5af4ece8afb9b84
jijjjj <13653419216@163.com>
2020-04-23 14:23:48 +0800

    场次显示

---
+++ Assets/Source/View/GameSessionItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class GameSessionItem : MonoBehaviour
+{
+    [SerializeField]
+    private Text m_gameIdText;
+    [SerializeField]
+    private Text m_gameTimeText;
+    [SerializeField]
+    private Text m_playerNumberText;
+    
+    public void SetSessionInfo(GameSessionInfo _vo)
+    {
+        m_gameIdText.text = _vo.game_id;
+        m_gameTimeText.text = _vo.game_time;
+    }
+}
